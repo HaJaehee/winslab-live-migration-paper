dd0e46be5b2bcf017fa288d2d9d316e7c1f5d9b0
J.Ha <jaehee.ha@kaist.ac.kr>
2020-03-23 19:01:21 +0900

    Updated for two interfaces of blue

--- conf-backup/blue/connect-jupiter.sh
+++ conf-backup/blue/connect-jupiter.sh
@@ -3,4 +3,5 @@
 wpa_cli select_network 0
 arp -i wlan0 -s 10.16.1.1 20:00:00:00:00:10
 arp -i wlan0 -s 10.64.0.2 20:00:00:00:00:10
-wpa_cli preauthenticate 20:00:00:00:00:10
+arping -w 0.05 -c 20 -f -I wlan0 10.16.1.1
+#ping -i 0.05 -c 80 -W 0.05 -w 4 10.16.1.1
--- conf-backup/blue/connect-moon.sh
+++ conf-backup/blue/connect-moon.sh
@@ -3,3 +3,4 @@
 wpa_cli select_network 2
 arp -i wlan0 -s 10.48.1.1 20:00:00:00:02:10
 arp -i wlan0 -s 10.64.0.2 20:00:00:00:02:10
+arping -w 0.01 -c 20 -f -I wlan0 10.48.1.1
