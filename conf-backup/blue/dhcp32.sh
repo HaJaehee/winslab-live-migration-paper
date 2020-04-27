046cff613a9800c0569dc745e097e0d8973726da
J.Ha <jaehee.ha@kaist.ac.kr>
2020-04-27 10:45:16 +0900

    Updated client wifi scripts

--- conf-backup/blue/dhcp32.sh
+++ conf-backup/blue/dhcp32.sh
@@ -9,8 +9,8 @@ ip link set dev wlan1 up
 sleep 5
 
 wpa_supplicant -B \
-      -i wlan0 -Dnl80211 -c wpa2.conf -N \
-      -i wlan1 -Dnl80211 -c wpa.conf
+      -i wlan0 -Dnl80211 -c wpa.conf -N \
+      -i wlan1 -Dnl80211 -c wpa2.conf
 ifconfig wlan0 10.32.1.2 netmask 255.0.0.0
 ifconfig wlan1 10.32.1.2 netmask 255.0.0.0
 arp -i wlan0 -s 10.32.1.1 20:00:00:00:01:10
